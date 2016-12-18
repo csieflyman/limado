4f9153fd8ed753911cfb8fb774de0361d3112672
flyman <csieflyman@gmail.com>
2016-12-18 15:44:49 +0800

    override getDescendants, getChildren method

--- collab/src/main/java/com/limado/collab/service/UserServiceImpl.java
+++ collab/src/main/java/com/limado/collab/service/UserServiceImpl.java
@@ -4,9 +4,12 @@
 
 package com.limado.collab.service;
 
+import com.google.common.base.Preconditions;
+import com.limado.collab.model.Party;
 import com.limado.collab.model.User;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -38,5 +41,19 @@ public class UserServiceImpl extends PartyServiceImpl<User> implements UserServi
     @Override
     public void removeChild(UUID parentId, UUID childId) {
         throw new UnsupportedOperationException(String.format("user %s can't remove child", parentId));
+    }
+
+    @Override
+    public Set<Party> getChildren(UUID id) {
+        Preconditions.checkArgument(id != null, "id must not be null");
+
+        return new HashSet<>();
+    }
+
+    @Override
+    public Set<Party> getDescendants(UUID id) {
+        Preconditions.checkArgument(id != null, "id must not be null");
+
+        return new HashSet<>();
     }
 }
